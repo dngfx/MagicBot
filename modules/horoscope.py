0ed58e9d25a17d869798c31a1fd0d9e44dcc60f7
dngfx <dngfx@dong.expert>
2020-07-12 02:10:17 +0100

    Add horoscopes!

---
+++ modules/horoscope.py
@@ -0,0 +1,39 @@
+#--depends-on commands
+
+import re, random
+from src import ModuleManager, utils
+
+STARSIGNS = [
+    "aries",
+    "taurus",
+    "gemini",
+    "cancer",
+    "leo",
+    "virgo",
+    "libra",
+    "scorpio",
+    "sagittarius",
+    "capricorn",
+    "aquarius",
+    "pisces"
+]
+
+STARSIGN_URL = "https://horoscope-api.herokuapp.com/horoscope/today/%s"
+
+
+class Module(ModuleManager.BaseModule):
+
+    @utils.hook("received.command.horoscope")
+    @utils.kwarg("help", "Get your daily horoscope")
+    @utils.kwarg("min_args", 1)
+    @utils.spec("!<starsign>lstring")
+    def wikipedia(self, event):
+        sign = event["spec"][0].lower()
+
+        if sign not in STARSIGNS:
+            starsigns = ", ".join(STARSIGNS)
+            event["stderr"].write("Invalid Starsigns. Valid starsigns are: %s" % starsigns)
+
+        page = utils.http.request(STARSIGN_URL % sign).json()
+
+        event["stdout"].write("Today's Horoscope for %s: %s" % (utils.irc.bold(sign.capitalize()), page["horoscope"]))
