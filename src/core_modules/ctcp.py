55b5a074de77350291106c5466fb3d489750eaab
dngfx <dngfx@dong.expert>
2020-07-12 10:38:17 +0100

    Update version information and delete trash.py

--- src/core_modules/ctcp.py
+++ src/core_modules/ctcp.py
@@ -9,7 +9,7 @@ class Module(ModuleManager.BaseModule):
 
     @utils.hook("received.ctcp.request.version")
     def ctcp_version(self, event):
-        default = "BitBot %s (%s)" % (IRCBot.VERSION, IRCBot.SOURCE)
+        default = "dongbot %s (%s)" % (IRCBot.VERSION, IRCBot.SOURCE)
 
         event["user"].send_ctcp_response("VERSION", self.bot.config.get("ctcp-version", default))
 
