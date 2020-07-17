f2730aa05a36f8b5388f7a709959a77856775a73
dngfx <294904+dngfx@users.noreply.github.com>
2020-07-17 23:54:13 +0100

    Update imgur to work properly.

--- modules/imgur.py
+++ modules/imgur.py
@@ -4,12 +4,13 @@
 
 import re, datetime
 from src import ModuleManager, utils, EventManager
+from hurry.filesize import size, alternative
 
 REGEX_IMAGE = re.compile("https?://(?:i\.)?imgur.com/(\w+)")
 REGEX_GALLERY = re.compile("https?://imgur.com/gallery/(\w+)")
 
-GALLERY_FORMAT = "%s%s%sA gallery with %s image%s, %s views, posted %s (%s%s)%s"
-IMAGE_FORMAT = "%s%s%sA %s image, %sx%s, with %s views, posted %s%s"
+GALLERY_FORMAT = "%s%s%sA gallery with %s image%s, %s views, posted %s%s"
+IMAGE_FORMAT = "%s%s%sA %s image, %s, %sx%s, with %s views, posted %s%s"
 
 URL_IMAGE = "https://api.imgur.com/3/image/%s"
 URL_GALLERY = "https://api.imgur.com/3/gallery/%s"
@@ -20,18 +21,10 @@ ARROW_DOWN = "↓"
 NSFW_TEXT = "(NSFW)"
 
 
-@utils.export("channelset",
-              utils.BoolSetting("auto-imgur",
-                                "Disable/Enable automatically getting info from Imgur URLs"))
+@utils.export(
+    "channelset", utils.BoolSetting("auto-imgur", "Disable/Enable automatically getting info from Imgur URLs")
+)
 class Module(ModuleManager.BaseModule):
-
-    def _prefix(self, data):
-        text = "%s: " % data["id"]
-        if data["nsfw"]:
-            text += "[NSFW] "
-        if data["account_url"]:
-            text += "%s " % data["account_url"]
-        return text
 
     @utils.hook("command.regex")
     @utils.kwarg("ignore_action", False)
@@ -39,7 +32,13 @@ class Module(ModuleManager.BaseModule):
     @utils.kwarg("pattern", REGEX_IMAGE)
     def _regex_image(self, event):
         if event["target"].get_setting("auto-imgur", False):
-            event["stdout"].write(self._parse_image(event["match"].group(1)))
+            if event["match"].group(1) == "gallery":
+                gallery_str = REGEX_GALLERY.match(event["match"].string)
+                self._parse_gallery(event, gallery_str.group(1))
+                event.eat()
+                return True
+
+            self._parse_image(event, event["match"].group(1))
             event.eat()
 
     @utils.hook("command.regex")
@@ -48,123 +47,116 @@ class Module(ModuleManager.BaseModule):
     @utils.kwarg("pattern", REGEX_GALLERY)
     def _regex_gallery(self, event):
         if event["target"].get_setting("auto-imgur", False):
-            event["stdout"].write(self._parse_gallery(event["match"].group(1)))
+            self._parse_gallery(event, event["match"].group(1))
             event.eat()
 
-    def _parse_gallery(self, hash):
+    def _parse_gallery(self, event, hash):
         api_key = self.bot.config["imgur-api-key"]
-        result = utils.http.request(URL_GALLERY % hash,
-                                    headers={
-                                        "Authorization": "Client-ID %s" % api_key
-                                    }).json()
+        result = utils.http.request(URL_GALLERY % hash, headers={"Authorization": "Client-ID %s" % api_key})
 
-        if result and result["success"]:
-            data = result["data"]
-            text = ""
+        if result.code == 404:
+            event["stderr"].write("Image %s not found." % hash)
+            return False
 
-            nsfw = utils.irc.bold(NSFW_TEXT) + " " if data["nsfw"] else ""
-            title = data["title"] + " " if data["title"] else ""
-            views = data["views"]
-            time = datetime.datetime.utcfromtimestamp(data["datetime"]). \
-                strftime("%e %b, %Y at %H:%M")
-            ups = utils.irc.color(str(data["ups"]) + ARROW_UP, utils.consts.GREEN)
-            downs = utils.irc.color(ARROW_DOWN + str(data["downs"]), utils.consts.RED)
-            images = data["images_count"]
-            image_plural = "" if images == 1 else "s"
+        result = result.json()
 
-            bracket_left = "(" if title or nsfw else ""
-            bracket_right = ")" if title or nsfw else ""
+        if not result or ("status" in result and result["status"] != 200):
+            event["stderr"].write("Error decoding response.")
+            return False
 
-            return GALLERY_FORMAT % (nsfw,
-                                     title,
-                                     bracket_left,
-                                     images,
-                                     image_plural,
-                                     views,
-                                     time,
-                                     ups,
-                                     downs,
-                                     bracket_right)
+        data = result["data"]
 
-    def _parse_image(self, hash):
+        nsfw = ("%s " % utils.irc.bold(NSFW_TEXT)) if data["nsfw"] == True else ""
+        title = ("%s " % data["title"]) if data["title"] else ""
+        views = data["views"]
+        time = datetime.datetime.utcfromtimestamp(data["datetime"]).strftime("%e %b, %Y at %H:%M")
+        images = data["images_count"]
+        image_plural = "" if images == 1 else "s"
+
+        bracket_left = "(" if title or nsfw else ""
+        bracket_right = ")" if title or nsfw else ""
+
+        output = GALLERY_FORMAT % (
+            nsfw, utils.irc.bold(title), bracket_left, utils.irc.bold(images), image_plural, utils.irc.bold(views),
+            utils.irc.bold(time), bracket_right
+        )
+
+        event["stdout"].write(output)
+
+    def _parse_image(self, event, hash):
         api_key = self.bot.config["imgur-api-key"]
-        result = utils.http.request(URL_IMAGE % hash,
-                                    headers={
-                                        "Authorization": "Client-ID %s" % api_key
-                                    }).json()
+        result = utils.http.request(URL_IMAGE % hash, headers={"Authorization": "Client-ID %s" % api_key})
 
-        if result and result["success"]:
-            data = result["data"]
-            text = ""
+        if result.code == 404:
+            event["stderr"].write("Image %s not found." % hash)
+            return False
 
-            nsfw = utils.irc.bold(NSFW_TEXT) + " " if data["nsfw"] else ""
-            title = data["title"] + " " if data["title"] else ""
-            type = data["type"].split("/")[-1]
-            width = data["width"]
-            height = data["height"]
-            views = data["views"]
-            time = datetime.datetime.utcfromtimestamp(data["datetime"]).\
-                strftime("%e %b, %Y at %H:%M")
+        result = result.json()
 
-            #%Y-%m-%d %H:%M:%S+00:00 (UTC)
+        if not result or ("status" in result and result["status"] != 200):
+            event["stderr"].write("Error decoding response.")
+            return False
 
-            bracket_left = "(" if title or nsfw else ""
-            bracket_right = ")" if title or nsfw else ""
+        data = result["data"]
 
-        return IMAGE_FORMAT % (nsfw, title, bracket_left, type, width, height, views, time, bracket_right)
+        nsfw = ("%s " % utils.irc.bold(NSFW_TEXT)) if data["nsfw"] == True else ""
+        title = ("%s " % data["title"]) if data["title"] else ""
+        views = data["views"]
+        time = datetime.datetime.utcfromtimestamp(data["datetime"]).strftime("%e %b, %Y at %H:%M")
+        mime = data["type"].split("/")[-1]
+        width = data["width"]
+        height = data["height"]
+        fsize = size(data["size"], system=alternative)
 
-    def _image_info(self, hash):
-        api_key = self.bot.config["imgur-api-key"]
-        result = utils.http.request(URL_IMAGE % hash,
-                                    headers={
-                                        "Authorization": "Client-ID %s" % api_key
-                                    }).json()
+        bracket_left = "(" if title or nsfw else ""
+        bracket_right = ")" if title or nsfw else ""
 
-        if result and result["success"]:
-            data = result["data"]
-            text = self._prefix(data)
+        output = IMAGE_FORMAT % (
+            nsfw, title, bracket_left, utils.irc.bold(mime), utils.irc.bold(fsize), width, height,
+            utils.irc.bold(views), utils.irc.bold(time), bracket_right
+        )
 
-            text += "(%s %dx%d, %d views)" % (data["type"], data["width"], data["height"], data["views"])
-            if data["title"]:
-                text += " %s" % data["title"]
-            return text
-        else:
-            return None
+        event["stdout"].write(output)
 
-    def _gallery_info(self, hash):
-        api_key = self.bot.config["imgur-api-key"]
-        result = utils.http.request(URL_GALLERY % hash,
-                                    headers={
-                                        "Authorization": "Client-ID %s" % api_key
-                                    }).json()
 
-        if result and result["success"]:
-            data = result["data"]
-            text = self._prefix(data)
-            text += "(%d views, %d??%d)" % (data["views"], data["ups"], data["downs"])
-            if data["title"]:
-                text += " %s" % data["title"]
-            return text
-        else:
-            return None
-
-    @utils.hook("received.command.imgur", min_args=1)
-    def imgur(self, event):
-        """
-        :help: Get information about a given imgur image URL
-        :usage: <url>
-        """
-        image_match = REGEX_IMAGE.match(event["args_split"][0])
-
-        result = None
-        if image_match:
-            result = self._image_info(image_match.group(1))
-        else:
-            gallery_match = REGEX_GALLERY.match(event["args_split"][0])
-            if gallery_match:
-                result = self._gallery_info(gallery_match.group(1))
-
-        if result:
-            event["stdout"].write(result)
-        else:
-            raise utils.EventResultsError()
+"""
+{
+    'data': {
+        'id': '7PiHitE',
+        'title': None,
+        'description': None,
+        'datetime': 1585303718,
+        'type': 'image/jpeg',
+        'animated': False,
+        'width': 823,
+        'height': 1279,
+        'size': 163954,
+        'views': 676,
+        'bandwidth': 110832904,
+        'vote': None,
+        'favorite': False,
+        'nsfw': False,
+        'section': None,
+        'account_url': None,
+        'account_id': None,
+        'is_ad': False,
+        'in_most_viral': False,
+        'has_sound': False,
+        'tags': [],
+        'ad_type': 0,
+        'ad_url': '',
+        'edited': '0',
+        'in_gallery': False,
+        'link': 'https://i.imgur.com/7PiHitE.jpg',
+        'ad_config': {
+            'safeFlags': ['not_in_gallery', 'share'],
+            'highRiskFlags': [],
+            'unsafeFlags': ['sixth_mod_unsafe'],
+            'wallUnsafeFlags': [],
+            'showsAds': False
+        }
+    },
+    'success': True,
+    'status': 200
+}
+"""
