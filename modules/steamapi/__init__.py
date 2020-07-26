29d3810822a8005e7f80bf7eb4bd1a16f7cfe70c
dfx the great <294904+dngfx@users.noreply.github.com>
2020-07-26 14:39:36 +0100

    Add last-seen and current game to steam profile summary.

--- modules/steamapi/__init__.py
+++ modules/steamapi/__init__.py
@@ -3,7 +3,7 @@
 #--depends-on permissions
 #--require-config steam-api-key
 
-import time, math, pprint
+import time, math, pprint, datetime
 from steam import webapi, steamid
 from steam.steamid import steam64_from_url, SteamID
 from steam.webapi import WebAPI
@@ -55,6 +55,8 @@ class Module(ModuleManager.BaseModule):
 
         summary = self.get_player_summary(steam_id)
 
+        print(summary)
+
         if not summary["players"]:
             event["stderr"].write("Could not find that user")
             return
@@ -64,49 +66,83 @@ class Module(ModuleManager.BaseModule):
         status = SteamConsts.user_state(summary["personastate"])
         steam_name = summary["personaname"]
 
-        message = "User Summary (%s): Status: %s" % (utils.irc.bold(steam_name), utils.irc.bold(status))
+        visibility = summary["communityvisibilitystate"]
+        if visibility == 1:
+            visibility = "Private"
+        elif visibility == 3:
+            visibility = "Public"
+
+        last_seen = ""
+
+        display_name = summary["realname"] if "realname" in summary else steam_name
+
+        if "lastlogoff" in summary:
+            pretty_time = utils.datetime.format.to_pretty_time(
+                (datetime.datetime.now() - datetime.datetime.fromtimestamp(summary["lastlogoff"])).total_seconds()
+            )
+            last_seen = " — Last Seen: %s ago" % utils.irc.bold(pretty_time)
+
+        currently_playing = ""
+
+        if "gameextrainfo" in summary:
+            currently_playing = " — Currently Playing: %s" % utils.irc.bold(summary["gameextrainfo"])
+
+        message = "%s (%s): Status: %s — Visibility: %s%s%s — Profile: %s" % (
+            steam_name,
+            utils.irc.bold(display_name),
+            utils.irc.bold(status),
+            utils.irc.bold(visibility),
+            last_seen,
+            currently_playing,
+            utils.irc.bold(summary["profileurl"])
+        )
 
         event["stdout"].write(message)
 
     def get_player_summary(self, id):
         return self.call("ISteamUser.GetPlayerSummaries", steamids=id)
 
-    """ {
-        'players':
-            [
-                {
-                    'steamid':
-                        '76561198028248833',
-                    'communityvisibilitystate':
-                        3,
-                    'profilestate':
-                        1,
-                    'personaname':
-                        'erica',
-                    'profileurl':
-                        'https://steamcommunity.com/id/ericathesnark/',
-                    'avatar':
-                        'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/5f/5f8655f3e24f3c1a27b2e38ffd2c968d8f28a594.jpg',
-                    'avatarmedium':
-                        'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/5f/5f8655f3e24f3c1a27b2e38ffd2c968d8f28a594_medium.jpg',
-                    'avatarfull':
-                        'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/5f/5f8655f3e24f3c1a27b2e38ffd2c968d8f28a594_full.jpg',
-                    'avatarhash':
-                        '5f8655f3e24f3c1a27b2e38ffd2c968d8f28a594',
-                    'lastlogoff':
-                        1595735237,
-                    'personastate':
-                        1,
-                    'realname':
-                        'Erica',
-                    'primaryclanid':
-                        '103582791464854615',
-                    'timecreated':
-                        1280276956,
-                    'personastateflags':
-                        0,
-                    'loccountrycode':
-                        'MX'
-                }
-            ]
-    } """
+
+""" {
+    'players':
+        [
+            {
+                'steamid':
+                    '76561198002971551',
+                'communityvisibilitystate':
+                    3,
+                'profilestate':
+                    1,
+                'personaname':
+                    'dongfix',
+                'commentpermission':
+                    1,
+                'profileurl':
+                    'https://steamcommunity.com/id/dngfx/',
+                'avatar':
+                    'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/88/88a1f6a28c60d6067b7b2f129a771660d0a68be3.jpg',
+                'avatarmedium':
+                    'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/88/88a1f6a28c60d6067b7b2f129a771660d0a68be3_medium.jpg',
+                'avatarfull':
+                    'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/88/88a1f6a28c60d6067b7b2f129a771660d0a68be3_full.jpg',
+                'avatarhash':
+                    '88a1f6a28c60d6067b7b2f129a771660d0a68be3',
+                'personastate':
+                    3,
+                'realname':
+                    'Dan',
+                'primaryclanid':
+                    '103582791429521408',
+                'timecreated':
+                    1227153609,
+                'personastateflags':
+                    0,
+                'loccountrycode':
+                    'GB',
+                'locstatecode':
+                    'C3',
+                'loccityid':
+                    16614
+            }
+        ]
+} """
