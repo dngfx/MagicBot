7318c0c4e90454c8660bcd6d1b22f6dd4f7ae349
dngfx <94904+dngfx@users.noreply.github.com>
2020-10-14 11:04:45 +0100

    Weather forecasts!

--- modules/weather.py
+++ modules/weather.py
@@ -3,8 +3,10 @@
 #--require-config openweathermap-api-key
 
 from src import ModuleManager, utils
+import datetime, time
 
 URL_WEATHER = "http://api.openweathermap.org/data/2.5/weather"
+URL_FORECAST = "http://api.openweathermap.org/data/2.5/forecast"
 
 class Module(ModuleManager.BaseModule):
 
@@ -99,3 +101,88 @@ class Module(ModuleManager.BaseModule):
                 event["stderr"].write("No weather information for this location")
         else:
             raise utils.EventResultsError()
+
+    @utils.hook("received.command.f", alias_of="forecast")
+    @utils.hook("received.command.forecast")
+    def forecast(self, event):
+        """
+        :help: Get current weather for you or someone else
+        :usage: [nickname]
+        :require_setting: location
+        :require_setting_unless: 1
+        """
+        api_key = self.bot.config["openweathermap-api-key"]
+
+        location = None
+        query = None
+        nickname = None
+        if event["args"]:
+            query = event["args"]
+            if len(event["args_split"]) == 1 and event["server"].has_user_id(event["args_split"][0]):
+                target_user = event["server"].get_user(event["args_split"][0])
+                location = self._user_location(target_user)
+                if location is not None:
+                    nickname = target_user.nickname
+            else:
+                location = event["args_split"][0]
+        else:
+            location = self._user_location(event["user"])
+            nickname = event["user"].nickname
+            if location is None:
+                raise utils.EventError("You don't have a location set")
+
+        args = {
+            "units": "metric",
+            "APPID": api_key
+        }
+
+        if location is not None and query:
+            location_info = self.exports.get_one("get-location")(query)
+            if location_info is not None:
+                location = [location_info["lat"], location_info["lon"], location_info.get("name", None)]
+        if location is None:
+            raise utils.EventError("Unknown location")
+
+
+        lat, lon, location_name = location
+        args["lat"] = lat
+        args["lon"] = lon
+        location_str = location_name
+
+        page = utils.http.request(URL_FORECAST, get_params=args).json()
+
+        if "list" not in page:
+            event["stderr"].write("No weather information for this location")
+            return
+
+        forecast = []
+        forecast_str = []
+        day_last = ""
+
+        forecast_list = page["list"]
+        for page in forecast_list:
+
+            dt = datetime.datetime.fromtimestamp(page["dt"])
+            day = datetime.datetime.strftime(dt, "%a")
+
+            if day_last == day:
+                continue
+            else:
+                day_last = day
+
+
+            celsius = "%dC" % page["main"]["temp"]
+            fahrenheit = "%dF" % ((page["main"]["temp"] * (9 / 5)) + 32)
+
+            # wind speed is in metres per second - 3.6* for KMh
+            wind_speed = 3.6 * page["wind"]["speed"]
+            wind_speed_k = "%sKMh" % round(wind_speed, 1)
+            wind_speed_m = "%sMPh" % round(0.6214 * wind_speed, 1)
+
+            forecast_day_str = "%s: %s/%s | Wind: %s/%s" % (utils.irc.bold(day), celsius, fahrenheit,
+                                                                 wind_speed_k, wind_speed_m)
+
+            forecast_str.append(forecast_day_str)
+
+        forecast_final = " — ".join(forecast_str)
+        event["stdout"].write("Forecast for %s: %s" % (location_str, forecast_final))
