244f4643dc6f8060c1ff77c3d623de62e358e7ab
dngfx <294904+dngfx@users.noreply.github.com>
2020-10-15 11:34:29 +0100

    Made $bojo dynamic, and added a $hug command to the misc module.

--- modules/misc.py
+++ modules/misc.py
@@ -13,11 +13,17 @@ BOJO_CHOICES = [
     "https://misconfigured.link/bojo/5.jpg",
     "https://misconfigured.link/bojo/6.jpg",
     "https://misconfigured.link/bojo/7.jpg",
+    "https://misconfigured.link/bojo/8.jpg",
+    "https://misconfigured.link/bojo/9.jpg",
+    "https://misconfigured.link/bojo/10.jpg",
 
 ]
+
+BOJO_URL = "https://misconfigured.link/bojo/index.php"
 # BORIS JOHNSON
 
 @utils.export("channelset", utils.BoolSetting("jerkcity-enabled", "Is the jerkcity command enabled?"))
+@utils.export("channelset", utils.BoolSetting("bojo-enabled", "Is the bojo command enabled?"))
 
 class Module(ModuleManager.BaseModule):
     JERKCITY_LIST = []
@@ -42,6 +48,27 @@ class Module(ModuleManager.BaseModule):
     @utils.hook("received.command.borisjohnson", channel_only=True)
     @utils.kwarg("help", "Receive an image of boris johnson")
     def bojo(self, event):
-        event["stdout"].prefix = "BORIS JOHNSON"
-        event["stdout"].write("%s" % utils.irc.bold(random.choice(BOJO_CHOICES)))
 
+        channel = event["target"]
+        if not channel.get_setting("bojo-enabled", False):
+            return
+
+        args = {
+            "imagelist": "true"
+        }
+
+        page = utils.http.request(BOJO_URL, get_params=args).json()
+
+        event["stdout"].prefix = "BORIS JOHNSON"
+        event["stdout"].write("%s" % utils.irc.bold(random.choice(page)))
+
+    @utils.hook("received.command.hug", channel_only=True)
+    @utils.kwarg("help", "hug or send a hug")
+    @utils.spec("?<ruser>user")
+    def hug_user(self, event):
+        channel = event["target"]
+        user = event["spec"][0]
+        if user is None:
+            user = event["user"].nickname
+
+        channel.send_message("\x01ACTION hugs %s\x01" % user)
