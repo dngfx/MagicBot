317be852ec31b738539f47f3e2e279ed4dbcbd7a
dngfx <294904+dngfx@users.noreply.github.com>
2020-10-16 07:59:38 +0100

    Rename some ancient files

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open("VERSION", "r") as version_file:
     version = version_file.read().strip()
 
 setuptools.setup(
-        name="bitbot",
+        name="magicbot",
         version=version,
-        scripts=["bitbotd",
-                 "bitbotctl"],
-        author="jesopo",
-        author_email="bitbot@jesopo.uk",
+        scripts=["magicbotd",
+                 "magicbotctl"],
+        author="dngfx",
+        author_email="email@misconfigured.link",
         description="Modular event-driven IRC bot",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/jesopo/bitbot",
+        url="https://github.com/dngfx/MagicBot",
         packages=setuptools.find_packages(),
         classifiers=[
             "Environment :: Console",
